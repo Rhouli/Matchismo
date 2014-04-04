4fb8cca8d51ecfd5ec2215291deb7e541a7213bc
Rhouli <ryan.houlihan90@gmail.com>
2014-04-04 16:35:41 -0700

    Updates

---
+++ Matchismo/PlayingCard.h
@@ -0,0 +1,21 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Ryan on 4/4/14.
+//  Copyright (c) 2014 Ryan Houlihan. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) NSUInteger rank;
+
++ (NSArray *)validSuits;
++ (NSArray *)rankStrings;
++ (NSUInteger)maxRank;
+
+@end
+
---
+++ Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Ryan on 4/4/14.
+//  Copyright (c) 2014 Ryan Houlihan. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
