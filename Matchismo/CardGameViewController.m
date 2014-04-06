3be3a9c957b188649537ee02cf565d5337a7e79b
rhouli <ryan.houlihan90@gmail.com>
2014-04-06 16:57:40 -0700

    meow

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,30 +7,50 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
 
+- (Deck *)deck {
+    if(!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
 - (void)setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    if (self.flipCount == 52){
+        self.flipsLabel.text = [NSString stringWithFormat:@"A new deck has been delt."];
+        _flipCount = 0;
+        _deck = [self createDeck];
+    }
     NSLog(@"flipCount changed to %d", self.flipCount);
 }
+
 - (IBAction)touchCardButton:(UIButton *)sender {
-    
     if ([sender.currentTitle length]){
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
-         [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
                           forState:UIControlStateNormal];
-        [sender setTitle:@"A" forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+            self.flipCount++;
+        }
     }
-    self.flipCount++;
 }
+
 @end
