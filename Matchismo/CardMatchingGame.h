1c5ea1dff3fb64857a829e1835d6db971b44d241
Rhouli <ryan.houlihan90@gmail.com>
2014-04-09 20:14:40 -0700

    pow pow meow meow

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,22 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Ryan on 4/9/14.
+//  Copyright (c) 2014 Ryan Houlihan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck;
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
